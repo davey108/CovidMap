e2b86cec813316503c8c9cd167265089477f0e2a
davey108 <sligar.david@gmail.com>
2020-07-13 02:06:57 -0400

    Added tests and fixed functionality

--- src/lambdafunctions/vdh-fetcher.js
+++ src/lambdafunctions/vdh-fetcher.js
@@ -13,13 +13,14 @@ AWS.config.getCredentials(err => {
     }
 });
 
-/*
-This function creates the folder for storing the vdh public use dataset cases if not exist
-@return a promise which contains the folder name for storing the dataset or reject if error
-*/
+/**
+ * This function creates the folder for storing the vdh public use dataset cases if not exist
+ * @return a promise which contains a JSON with fields name and text, where name contains the folder's key and text describe created items
+ */
 let createStorageFolders = async () => {
     const bucketName = "vdh-dataset";
-    const vdhPublicDataSetFolder = "Public-Dataset-Cases"
+    const vdhPublicDataSetFolder = "Public-Dataset-Cases";
+    let text = [];
     try{
         let data = await s3.listBuckets({}).promise();
         if ((data.Buckets.filter(bucketDetails => bucketDetails.Name === bucketName)).length === 0){
@@ -29,13 +30,20 @@ let createStorageFolders = async () => {
                 ACL: 'private'
             }
             await s3.createBucket(createBucketParams).promise();
-            // check if the file is there
-            // the / indicates it is a folder in S3 object
-            let getFolderResult = await s3.getObject({Bucket: bucketName, Key: bucketName + "/"}).promise();
+            text.push("Bucket")
         }
+        // check if the file is there
+        // the / indicates it is a folder in S3 object
+        await s3.getObject({Bucket: bucketName, Key: vdhPublicDataSetFolder + "/"}).promise();
+        return {name: vdhPublicDataSetFolder + "/", text: text};
     }
     catch(err){
-        
+        // catches if getting folder does not exist, then creates the folder
+        if(err.code === "NoSuchKey"){
+            await s3.putObject({Bucket: bucketName, Key: vdhPublicDataSetFolder + "/", ACL: 'private'}).promise();
+            text.push("Folder")
+            return {name: vdhPublicDataSetFolder + "/", text: text};
+        }
         throw err;
     }
 
