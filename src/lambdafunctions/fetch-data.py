051c75a1f0098c4ecf2a98d2cb03dec7cfd8f24c
Abbas Beydoun <abbasbeydoun@pop-os.localdomain>
2020-07-12 21:18:26 -0400

    - Used high level resource API instead of low level client API - Better JSON format returned - Dynamodb returns all 'Number' columns as Decimal - Subclassed JSON encoder to deal with issue

--- src/lambdafunctions/fetch-data.py
+++ src/lambdafunctions/fetch-data.py
@@ -1,30 +1,31 @@
 import boto3
 import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
 
+# Subclassing json encoder to deal with dynamodbs returned decimal results
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return int(o)
+        return json.JSONEncoder.default(self,o)
 
 def lambda_handler(event, context):
-    client = boto3.client('dynamodb')
+    table = boto3.resource('dynamodb').Table('USA')
     if 'StateFIPS' in event:
-        if 'CountyFIPS' in event and event['CountyFIPS'] != 'empty':
-            key_condition_expression = 'StateFIPS = :StateFIPS AND CountyFIPS = :CountyFIPS'
-            expression_attribute_values = { ':StateFIPS' : {'S': event['StateFIPS']}, ':CountyFIPS' : {'S': event['CountyFIPS']} }
-        else:
-            key_condition_expression = 'StateFIPS = :StateFIPS'
-            expression_attribute_values = { ':StateFIPS' : {'S': event['StateFIPS']} }
-
-        response = client.query(
-        TableName='USA',
-        KeyConditionExpression=key_condition_expression,
-        ExpressionAttributeValues=expression_attribute_values)
-
+        key_condition_expression = Key('StateFIPS').eq(event['StateFIPS']) & Key('CountyFIPS').eq(event['CountyFIPS']) if 'CountyFIPS' in event and event['CountyFIPS'] != 'empty' else Key('StateFIPS').eq(event['StateFIPS'])
+        response = table.query(
+        KeyConditionExpression=key_condition_expression
+        )
         return {
             'statusCode': 200,
-            'body': json.dumps(response['Items'])
+            'body': json.dumps(response['Items'], cls=DecimalEncoder)
         }
-
     return {
         'statusCode': 400,
         'body': json.dumps('{"ERROR_MSG": "Mandatory key StateFIPS must be present in input JSON"}')
     }
 
-print(lambda_handler({"StateFIPS": "51", "CountyFIPS": "empty"}, None))
+
+#sys.exit(
+print(lambda_handler({"StateFIPS": "51", "CountyFIPS": "059"}, None))
