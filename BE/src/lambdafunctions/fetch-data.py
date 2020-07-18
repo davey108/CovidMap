2c0da37ae3801e73833041c4afa053f0e434994a
abbasbeydoun <abbasbeydoun95@gmail.com>
2020-07-18 02:22:49 -0400

    - Created API Gateway which calls lambdafunction, modified script to handle no input at all scenario and event format

--- BE/src/lambdafunctions/fetch-data.py
+++ BE/src/lambdafunctions/fetch-data.py
@@ -17,24 +17,30 @@ def lambda_handler(event, context):
     @return: The number of cases and deaths for all counties in a state or a specific county
     """
     table = boto3.resource('dynamodb').Table('United_States')
-    if 'FIPS' in event:
-        # TODO: Decide on defaults for start_date and end_date
-        if 'start_date' in event or 'end_date' in event:
-            key_condition_expression = 'FIPS = :FIPS AND Date BETWEEN :start_date AND :end_date'
-            expression_attribute_values = {':FIPS': event['FIPS'], ':start_date': event['start_date'], ':end_date': event['end_date']}
-        else:
-            key_condition_expression = 'FIPS = :FIPS'
-            expression_attribute_values = {':FIPS': event['FIPS']}
+    if 'multiValueQueryStringParameters' in event:
+        query_string_parameters = event['multiValueQueryStringParameters']
+        if 'FIPS' in query_string_parameters:
+            # TODO: Decide on defaults for start_date and end_date
+            if 'start_date' in query_string_parameters or 'end_date' in query_string_parameters:
+                key_condition_expression = 'FIPS = :FIPS AND Date BETWEEN :start_date AND :end_date'
+                expression_attribute_values = {':FIPS': query_string_parameters['FIPS'][0], ':start_date': query_string_parameters['start_date'][0], ':end_date': query_string_parameters['end_date'][0]}
+            else:
+                key_condition_expression = 'FIPS = :FIPS'
+                expression_attribute_values = {':FIPS': query_string_parameters['FIPS'][0]}
 
-        response = table.query(
-        KeyConditionExpression=key_condition_expression,
-        ExpressionAttributeValues=expression_attribute_values
-        )
+            response = table.query(
+            KeyConditionExpression=key_condition_expression,
+            ExpressionAttributeValues=expression_attribute_values
+            )
+            return {
+                'statusCode': 200,
+                'body': json.dumps(response['Items'], cls=DecimalEncoder)
+            }
         return {
-            'statusCode': 200,
-            'body': json.dumps(response['Items'], cls=DecimalEncoder)
+            'statusCode': 400,
+            'body': json.dumps('{"ERROR_MSG": "Mandatory key FIPS must be present in query params"}')
         }
     return {
         'statusCode': 400,
-        'body': json.dumps('{"ERROR_MSG": "Mandatory key FIPS must be present in input JSON"}')
+        'body': json.dumps('{"ERROR_MSG": "GET Request must provide query string parameters"}')
     }
